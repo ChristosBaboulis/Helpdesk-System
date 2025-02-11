1686ab3217d95dc658571a92358b4bf53192e349
Christos Bampoulis <christos.bampoulis@Christoss-MacBook-Air-2.local>
2025-02-12 01:01:44 +0200

    Added some test classes

---
+++ src/test/java/com/example/helpdesk/persistence/Initializer.java
@@ -0,0 +1,36 @@
+package com.example.helpdesk.persistence;
+
+import com.example.helpdesk.domain.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class Initializer {
+
+    private EntityManager em;
+
+    public Initializer() {
+        em = JPAUtil.getCurrentEntityManager();
+    }
+
+    private void eraseData() {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+//        em.createNativeQuery("delete from reviews").executeUpdate();
+//        em.createNativeQuery("delete from review_invitations").executeUpdate();
+//        em.createNativeQuery("delete from article_authors").executeUpdate();
+//        em.createNativeQuery("delete from articles").executeUpdate();
+//        em.createNativeQuery("delete from journals").executeUpdate();
+//        em.createNativeQuery("delete from users").executeUpdate();
+//        em.createNativeQuery("delete from authors").executeUpdate();
+
+        tx.commit();
+    }
+
+    public void prepareData() {
+
+        eraseData();
+
+    }
+
+}
