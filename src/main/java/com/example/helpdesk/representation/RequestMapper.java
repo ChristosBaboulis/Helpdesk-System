8601eec6280691fa32156d655e5d1e819bd67153
ChristosBaboulis <chrisb2603@gmail.com>
2025-03-14 15:49:20 +0200

    Fix on RequestMapper

--- src/main/java/com/example/helpdesk/representation/RequestMapper.java
+++ src/main/java/com/example/helpdesk/representation/RequestMapper.java
@@ -11,9 +11,9 @@ import org.mapstruct.Mapping;
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         uses = {CustomerMapper.class, RequestCategoryMapper.class, CustomerSupportMapper.class, TechnicianMapper.class})
 public abstract class RequestMapper {
-    @Mapping(target = "actions", ignore = true)
     public abstract RequestRepresentation toRepresentation(Request request);
 
+    @Mapping(target = "actions", ignore = true)
     public abstract Request toModel(RequestRepresentation representation);
 
     public abstract List<RequestRepresentation> toRepresentationList(List<Request> request);
