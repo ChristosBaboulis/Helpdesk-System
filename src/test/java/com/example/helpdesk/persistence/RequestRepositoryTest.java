a7e52d63b36c9dabff7b3e6a0d3f616bb658e6dd
ChristosBaboulis <chrisb2603@gmail.com>
2025-03-16 16:23:00 +0200

    Updated persistence tests

--- src/test/java/com/example/helpdesk/persistence/RequestRepositoryTest.java
+++ src/test/java/com/example/helpdesk/persistence/RequestRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.example.helpdesk.persistence;
 
+import com.example.helpdesk.Fixture;
 import com.example.helpdesk.domain.Request;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,11 +15,17 @@ public class RequestRepositoryTest {
     RequestRepository requestRepository;
 
     @Test
-    public void test(){
+    public void testFindByTelephoneNumber(){
         List<Request> requests = requestRepository.findByTelephoneNumber("1234567890");
-        requests.forEach(r -> System.out.println("Request: " + r.getTelephoneNumber()));
+        Assertions.assertEquals(Fixture.Requests.PHONE_NUMBER, requests.get(0).getTelephoneNumber());
 
         Request r2 = requestRepository.findByTelephoneNumber("1234567890").getFirst();
-        System.out.println("Request 2: " + r2.getTelephoneNumber());
+        Assertions.assertEquals(Fixture.Requests.PHONE_NUMBER, r2.getTelephoneNumber());
+    }
+
+    @Test
+    public void testSearch(){
+        Request request = requestRepository.search(6000);
+        Assertions.assertEquals(6000, request.getId());
     }
 }
