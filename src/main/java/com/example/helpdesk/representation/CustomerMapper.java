ab82ab4cab04a4acc70c320ea848f0de8cbe84fd
ChristosBaboulis <chrisb2603@gmail.com>
2025-03-17 10:58:25 +0200

    CustomerMapper Test

--- src/main/java/com/example/helpdesk/representation/CustomerMapper.java
+++ src/main/java/com/example/helpdesk/representation/CustomerMapper.java
@@ -1,8 +1,11 @@
 package com.example.helpdesk.representation;
 
 import com.example.helpdesk.domain.Customer;
+import com.example.helpdesk.domain.PersonalInfo;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -14,4 +17,31 @@ public abstract class CustomerMapper {
     public abstract Customer toModel(CustomerRepresentation representation);
 
     public abstract List<CustomerRepresentation> toRepresentationList(List<Customer> customer);
+
+    @AfterMapping
+    public void fillPersonalInfo(CustomerRepresentation representation, @MappingTarget Customer customer) {
+        PersonalInfo personalInfo = new PersonalInfo();
+        personalInfo.setFirstName(representation.firstName);
+        personalInfo.setLastName(representation.lastName);
+        personalInfo.setEmailAddress(representation.emailAddress);
+        personalInfo.setTelephoneNumber(representation.telephoneNumber);
+        personalInfo.setBirthdate(representation.birthdate);
+        personalInfo.setAddress(representation.address);
+
+        customer.setPersonalInfo(personalInfo);
+    }
+
+    @AfterMapping
+    public void extractPersonalInfo(@MappingTarget CustomerRepresentation representation, Customer customer) {
+        if (customer.getPersonalInfo() != null) {
+            representation.firstName = customer.getPersonalInfo().getFirstName();
+            representation.lastName = customer.getPersonalInfo().getLastName();
+            representation.emailAddress = customer.getPersonalInfo().getEmailAddress();
+            representation.telephoneNumber = customer.getPersonalInfo().getTelephoneNumber();
+            representation.birthdate = customer.getPersonalInfo().getBirthdate();
+            representation.address = customer.getPersonalInfo().getAddress();
+        } else {
+            System.out.println("personalInfo is NULL when mapping Customer → CustomerRepresentation!");
+        }
+    }
 }
